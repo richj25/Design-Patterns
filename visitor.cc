5058b6d7600e2d3252f747aabcd795b7811a2e11
Richard Johnson <richj25@comcast.net>
2017-10-24 21:14:19 -0600

    added text for visitor

--- visitor.cc
+++ visitor.cc
@@ -1,5 +1,8 @@
 #include <iostream>
-#define ArrayLength 10
+#include <list>
+#include <vector>
+
+#define DataLength 10
 
 
 class Visitor
@@ -9,6 +12,45 @@ public:
 
 };
 
+class AverageVisitor : public Visitor
+{
+public:
+	AverageVisitor();
+
+	void visit(int ele);
+
+	float value();
+
+	void clear();
+
+private:
+	int numberOfElements;
+	int total;
+};
+
+AverageVisitor::AverageVisitor()
+{
+	numberOfElements = 0;
+	total = 0;
+}
+
+void AverageVisitor::visit(int ele)
+{
+	total += ele;
+	numberOfElements++;
+}
+
+float AverageVisitor::value()
+{
+	return (float)total/(float)numberOfElements;
+}
+
+void AverageVisitor::clear()
+{
+	total = 0;
+	numberOfElements = 0;
+}
+
 class SumVisitor : public Visitor
 {
 public:
@@ -17,6 +59,8 @@ public:
 	void visit(int ele);
 
 	int value();
+
+	void clear();
 
 private:
 	int total;
@@ -37,6 +81,11 @@ int SumVisitor::value()
 	return total;
 }
 
+void SumVisitor::clear()
+{
+	total = 0;
+}
+
 class Visitable
 {
 public:
@@ -48,37 +97,74 @@ class ArrayVisitable : public Visitable
 public:
 	ArrayVisitable();
 	void accept(Visitor &v);
-	static int length;
+
 
 private:
-	int data[ArrayLength];
+	std::vector<int> data;
 };
-
-int ArrayVisitable::length = ArrayLength;
 
 ArrayVisitable::ArrayVisitable()
 {
-	for (int i = 0; i < length; i++)
+	for (int i = 0; i < DataLength; i++)
 	{
-		data[i] = i;
+		data.push_back(i);
 	}
 }
 
 void ArrayVisitable::accept(Visitor &v)
 {
-	for (int i = 0; i < length; i++)
+	for (std::vector<int>::iterator iter = data.begin(); iter != data.end(); iter++)
 	{
-		v.visit(data[i]);
+		v.visit(*iter);
+	}
+}
+
+class ListVisitable : public Visitable
+{
+public:
+	ListVisitable();
+	void accept(Visitor &v);
+
+private:
+	std::list<int> data;
+};
+
+ListVisitable::ListVisitable()
+{
+	for (int i = 0; i < DataLength; i++)
+	{
+		data.push_back(i);
+	}
+}
+
+void ListVisitable::accept(Visitor &v)
+{
+	for (std::list<int>::iterator iter = data.begin(); iter != data.end(); iter++)
+	{
+		v.visit(*iter);
 	}
 }
 
 int main(void)
 {
 	ArrayVisitable array;
-	SumVisitor visitor;
+	ListVisitable list;
+	SumVisitor sum;
+	AverageVisitor average;
 
-	array.accept(visitor);
+	array.accept(sum);
+	std::cout << "Total is " << sum.value() << std::endl;
 
-	std::cout << "Total is " << visitor.value() << std::endl;
+	sum.clear();
 
+	list.accept(sum);
+	std::cout << "Total is " << sum.value() << std::endl;
+
+	array.accept(average);
+	std::cout << "Average is " << average.value() << std::endl;
+
+	average.clear();
+
+	list.accept(average);
+	std::cout << "Average is " << average.value() << std::endl;
 }
